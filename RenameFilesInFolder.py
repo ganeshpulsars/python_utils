3db935077de23ee4c3ae30ec359460fea24db1b4
Ganesh <ganeshpulsars@gmail.com>
2024-09-07 23:07:24 +0530

    * debug and error print statements added to RenameFilesInFolder

--- RenameFilesInFolder.py
+++ RenameFilesInFolder.py
@@ -1,5 +1,6 @@
 import math
 from pathlib import Path
+from .cprintUtils import dprint, eprint, iprint  # noqa: F401
 
 
 def RenameFilesInFolder(folder, FileNamePrefix=None):
@@ -10,13 +11,18 @@ def RenameFilesInFolder(folder, FileNamePrefix=None):
     Files in the first level alone are renamed. Files under sub-folders
     (if any) are not renamed
     """
+    # dprint("Welcome to RenameFilesInFolder")
     rename_list = []
     try:
         if isinstance(folder, str):
             folder = Path(folder)
 
         if not folder.is_dir():
+            eprint(f"{folder} is not a folder")
             raise Exception(f"{folder} is not a folder")
+        else:
+            # dprint(f"{folder} is a folder")
+            pass
 
         if FileNamePrefix is not None:
             prefix = FileNamePrefix
@@ -42,8 +48,10 @@ def RenameFilesInFolder(folder, FileNamePrefix=None):
                 old_fileName.rename(new_fullFileName)
                 rename_list.append((file, new_fileName))
     except Exception as e:
+        eprint(str(e))
         raise e
     finally:
+        # dprint("Come to finally")
         with open(root / (prefix + ".csv"), "a") as csv_file:
             for file in rename_list:
                 csv_file.write(f"{file[0]}, {file[1]}\n")
